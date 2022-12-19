c396a3146fca0fe0bf4f2e46a8b7c3312c6ca42f
Студент Академии <std@itstep.loc>
2022-12-19 20:49:14 +0300

    валидация

--- src/elements/form.js
+++ src/elements/form.js
@@ -1,6 +1,5 @@
 import "./form.css";
 import { useState } from 'react'
-import InputMask from 'react-input-mask';
 import PhoneInput from './input-phone';
 import Thanks from './thanks';
 import { api } from '../api'
@@ -8,15 +7,12 @@ import { api } from '../api'
 function Form(props) {
     const [sendQuantity, getSendQuantity] = useState(1)
     const [sendPhone, getSendPhone] = useState('');
-    // const [sendTariff, getSendTariff] = useState('')
-    // const [sendDate, getSendDate] = useState("2023-05-01")
-    // const [sendComment, getSendComment] = useState("Напишите Ваш комментарий")
     const [showThanks, setshowThanks] = useState(false)
     const [ isErrorName, setErrorName ] = useState(false)
-    const [ errorQuantity, setErrorQuantity ] = useState(false)
-    const [ errorPhone, setErrorPhone ] = useState(false)
-    const [ errorTariff, setErrorTariff ] = useState(false)
-    const [ errorDate, setErrorDate ] = useState(false)
+    const [ isErrorPhone, setErrorPhone ] = useState(false)
+    const [ isErrorQuantity, setErrorQuantity ] = useState(false)
+    const [ isErrorTariff, setErrorTariff ] = useState(false)
+    const [ isErrorDate, setErrorDate ] = useState(false)
 
     function handleThanks(num) {
         setshowThanks(num)
@@ -45,48 +41,94 @@ function Form(props) {
         })
     }
 
-    function validName() {
-        console.log('ошибка по умолчанию - ' + isErrorName)
-        sendList.name === "" ?  setErrorName(true) : setErrorName(false); 
-        console.log('появилась ошибка? - ' + isErrorName)
-        addSendlist()
-        return isErrorName
+    function notValidName() {
+        const res = sendList.name === "" ? true : false;
+        setErrorName(res);
+
+        return res
     }
 
-    function validQuantity() {
-        errorQuantity  = (sendList.quantity === "") ? setErrorQuantity(true) : setErrorQuantity(false)
-        return errorQuantity
+    function notValidQuantity() {
+        const res = sendList.quantity === "0" ? true : false;
+        setErrorQuantity(res);
+
+        return res
     }
 
-    function validPhone() {
-        const arr = sendList.phone.split('')
-        const newArr = arr.filter(item => (item != ' ') && (item != '(') && (item != ')')  && (item != '_') && (item != '+'))
-        console.log(newArr.length)
-        newArr.length != 11 ? setErrorPhone(true) : setErrorPhone(false)
-        console.log(errorPhone)
+    function notValidPhone() {
+        const phone = sendList.phone
+            .replace('+', '')
+            .replace('(', '')
+            .replace(')', '')
+            .replace(/_+/g, '')
+            .replace(/\s+/g, '');
+
+        const res = phone.length !== 11;
+
+        setErrorPhone(res);
+
+        return res;
+       
     }
 
-    function validTariff() {
-        errorTariff  = (sendList.tariff === "") ? setErrorTariff(true) : setErrorTariff(false)
-        return errorTariff
+    function notValidTariff() {
+        const res = sendList.tariff === "" ? true : false;
+        setErrorTariff(res);
+
+        return res
     }
 
-    function validDate() {
-        errorDate  = (sendList.date === "Выберете дату тура") ? setErrorDate(true) : setErrorDate(false)
-        return errorDate
+    function notValidDate() {
+        const res = sendList.date === "" ? true : false;
+        setErrorDate(res);
+
+        return res
     }
+
 
     function addSendlist(event) {
         event.preventDefault()
-        validName()
-        if (isErrorName) {
-            console.log(`Есть ошибка?  + ${isErrorName}`)
-        } else {
-            sendRequest(sendList)
-            handleThanks(true)
-            props.closeForm1()
-            console.log(sendList)
+
+        const errorName = notValidName()
+        const errorQuantity = notValidQuantity()
+        const errorPhone = notValidPhone()
+        const errorTariff = notValidTariff()
+        const errorDate = notValidDate()
+        
+        if (errorName) {
+            console.log(`name is not valid`)
+
+            return;
         }
+
+        if (errorQuantity) {
+            console.log(`name is not valid`)
+
+            return;
+        }
+
+        if (errorPhone) {
+            console.log(`phone is not valid`)
+
+            return;
+        }
+
+        if (errorTariff) {
+            console.log(`phone is not valid`)
+
+            return;
+        }
+
+        if (errorDate) {
+            console.log(`phone is not valid`)
+
+            return;
+        }
+
+        sendRequest(sendList)
+        handleThanks(true)
+        props.closeForm1()
+        console.log(sendList)
         
     }
 
@@ -105,7 +147,6 @@ function Form(props) {
                         type='text' 
                         placeholder="Имя"
                         className="form__name-input" 
-                        // required
                         onChange={(event) => handleField(event, 'name')} 
                     />
                     <p className={`form__error-text + ${isErrorName ? "show" : "hide"}`}>Укажите Ваше Имя</p>
@@ -122,7 +163,7 @@ function Form(props) {
                         className="form__number-input" 
                         onChange={(event) => handleField(event, 'quantity')}
                     ></input>
-                    <p className={errorQuantity ? "form__error-text show" : "form__error-text hide"}>Укажите колличество участников</p>
+                    <p className={`form__error-text + ${isErrorQuantity ? "show" : "hide"}`}>Укажите колличество участников</p>
                 </label>
             </div>
 
@@ -130,18 +171,18 @@ function Form(props) {
                 <label className="form__tel">
                     <p className="form__tel-title">Контактный телефон</p>
                     <PhoneInput value={sendPhone} onChange={handlePhone}/>
-                    <p className={errorPhone ? "form__error-text show" : "form__error-text hide"}>Введите номер телефона</p>
+                    <p className={`form__error-text + ${isErrorPhone ? "show" : "hide"}`}>Введите номер телефона</p>
                 </label>
 
                 <label className="form__date">
                 <p className="form__date-title">Даты тура</p>
                 <select name="dateTour" onChange={(event) => handleField(event, 'date')}>
-                    <option value="">Выберете дату тура</option>
+                    <option value="">Выберите дату тура</option>
                     <option value="23.06 - 30.06">23.06 - 30.06</option>
                     <option value="14.07 - 21.07">14.07 - 21.07</option>
                     <option value="20.08 - 27.08">20.08 - 27.08</option>
                 </select>
-                <p className={errorDate ? "form__error-text show" : "form__error-text hide"}>Укажите желаемую дату тура</p>
+                <p className={`form__error-text + ${isErrorDate ? "show" : "hide"}`}>Укажите желаемую дату тура</p>
                 </label>
             </div>
 
@@ -176,7 +217,7 @@ function Form(props) {
                     <p>179 000 руб</p>
                     </div>
                 </label>
-                <p className={errorTariff ? "form__error-text show" : "form__error-text hide"}>Укажите тариф</p>
+                <p className={`form__error-text + ${isErrorTariff ? "show" : "hide"}`}>Укажите тариф</p>
             </div>
 
             <label className="form__comment">
